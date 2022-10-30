926116fc0c8ad85ebac8611df1e38c841ba297e9
Алина <112811472+Alnpchn@users.noreply.github.com>
2022-10-30 17:33:27 +0300

    Update сам 5.c

--- сам 5.c
+++ сам 5.c
@@ -2,9 +2,14 @@
 #include<locale.h>
 #include<math.h>
 #define _USE_MATH_DEFINES
-void main()
+double name (double res1, double res2)
 {
-	double x, y, a, res1, res2;
+	double result = res1 / res2;
+	return result;
+}
+int main()
+{
+	double x, y, a, res1, res2, result1;
 	a = 7.1e-9;
 	setlocale(LC_ALL, "RUS");
 	puts("введите x");
@@ -12,6 +17,8 @@ void main()
 	puts("введите y");
 	scanf("%lf", &y);
 	res1 = cos(pow(x, 3) + 6) - sin(y - a);
-	res2 = log(pow(x, 4)) - 2*pow(sin (x), 5);
-	printf("Значение F=%lf", res1 / res2);
+	res2 = log(pow(x, 4)) - 2 * pow(sin(x), 5);
+	result1 = name(res1, res2);
+	printf("Значение F=%lf", result1);
+	return 0;
 }
